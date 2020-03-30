0e5767ef254ad42151d2ea0a2dd4c6b4e52d992b
Michael France Nelson <michaelfrancenelson@gmail.com>
2020-03-29 22:07:42 -0400

    chapter 6

--- data/environment_vars.R
+++ data/environment_vars.R
@@ -1,4 +1,8 @@
 book_data = "/data/Fletcher_Fortin-2018-Supporting_Files/data/"
+book_code = here::here("data", "author_code")
+
+
+dir.exists(book_code)
 
 # Store data locally:
 # dir.exists(book_data)
